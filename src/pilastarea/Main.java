407307406cca1aa8021bac8216dcf8e53993162f
AleMendezS <98509409+AleMendezS@users.noreply.github.com>
2022-10-05 02:04:43 -0600

    Participacion de Alejandro Mendez

--- src/pilastarea/Main.java
+++ src/pilastarea/Main.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pilastarea;
 
-/**
- *
- * @author David Alfaro
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
         Pila miPila = new Pila();
@@ -35,20 +27,7 @@ public class Main {
         miPila.listar();
         
         
-        
-        /*System.out.println("PILA ANTES DEL POP");
-        miPila.listar();
-        
-        miPila.pop();
-        
-        System.out.println("PILA DESPUES DE UN POP");
-        miPila.listar();
-        
-        
-        //miPila.popModificado(89);
-        System.out.println("PILA DESPUES DE UN DELETE A LA REFERENCIA 89");
-        miPila.listar();
-        */
+       
     }
     
 }
